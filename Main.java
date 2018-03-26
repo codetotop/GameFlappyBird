6d2914eec27f391e5ec34a68f2e6b8827648215e
codetotop <codetotop@users.noreply.github.com>
2018-03-26 15:32:04 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.codetotop.main;
+
+import com.codetotop.gui.MyFrame;
+
+/**
+ * Created by Dung96 on 2/28/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame();
+    }
+}
